1a952c6664a257ca91836152d94c265bdf556458
leatheronly <jacob.nofziger@gmail.com>
2017-02-02 01:46:45 -0500

    Fixed changing backgrounds and output of weather summary, made small fixes to grammar

--- get-weather.js
+++ get-weather.js
@@ -14,18 +14,27 @@ $(document).ready(function(){
 		}
 	});
 
+	// http://ip-api.com/json
+
 	function getWeather(units){	
+		var windLabel = ""; 
+		var tempLabel = "";
+		if(units === "imperial"){windLabel = " miles per hour"; tempLabel = " Farenheit";}
+		else{ windLabel = " meters per second"; tempLabel = " Celsius";};
 		// get weather
 		if(navigator.geolocation){
 			navigator.geolocation.getCurrentPosition(function(position){
+				
+
 				// pass position.coords.latitude and position.coords.longitude to the open weather api
 				var weatherCall = "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&APPID=79fa4289db999b8e15c0f7dcb061b6db&units=" + units;
 				$.getJSON(weatherCall, function(json){
-					// take response, filter for what you want, then print to the appropriate tags 
-					$("#location").html(json.name + " can expect:");
-					$("#condition").html(json.weather[0].description);
-					$("#wind").html("with wind speeds of " + json.wind.speed);
-					$("#temp").html(json.main.temp);
+					
+					$("#theReport").html("Expect <b>" + json.weather[0].main + "</b> in " + json.name + " today with wind speeds of " +json.wind.speed + windLabel);
+
+					$("#temp").html(json.main.temp + tempLabel);
+					var iconSrc = "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png";
+					$("#icon").attr("src", iconSrc);
 					temperature = json.main.temp;
 
 					if(temperature < 32){
